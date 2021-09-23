7f8c7928103880f17a5204d5edfff93633e5e047
Robert Tabb <wrtabb@gmail.com>
2021-09-23 11:22:56 -0500

    updated makeStackPlots.C

--- makeStackPlots.C
+++ makeStackPlots.C
@@ -1,57 +1,59 @@
 
 vector<TString> file_data = {
-	"output_data/DYHists_v2p6_Data_RunB_EE.root",
-	"output_data/DYHists_v2p6_Data_RunC_EE.root",
-	"output_data/DYHists_v2p6_Data_RunD_EE.root",
-	"output_data/DYHists_v2p6_Data_RunE_EE.root",
-	"output_data/DYHists_v2p6_Data_RunF_EE.root",
-	"output_data/DYHists_v2p6_Data_RunG_EE.root",
-	"output_data/DYHists_v2p6_Data_RunHver2_EE.root",
-	"output_data/DYHists_v2p6_Data_RunHver3_EE.root"
+	"output_data/crab_DoubleEG_RunB.root",           // 0
+	"output_data/crab_DoubleEG_RunC.root",           // 1
+	"output_data/crab_DoubleEG_RunD.root",           // 2
+	"output_data/crab_DoubleEG_RunE.root",           // 3
+	"output_data/crab_DoubleEG_RunF.root",           // 4
+	"output_data/crab_DoubleEG_RunG.root",           // 5
+	"output_data/crab_DoubleEG_RunHver2.root",       // 6
+	"output_data/crab_DoubleEG_RunHver3.root",       // 7
 };
 
 vector<TString> file_diboson = {
-	"output_data/DYHists_v2p6_Diboson_WW_EE.root",
-	"output_data/DYHists_v2p6_Diboson_WZ_EE.root",
-	"output_data/DYHists_v2p6_Diboson_ZZ_EE.root"
+	"output_data/WW.root",
+	"output_data/WZ.root",
+	"output_data/ZZ.root"
 };
 
 vector<TString> file_taus = {
-	"output_data/DYHists_v2p6_TauTau_DYLL_M100to200_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M200to400_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M400to500_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M500to700_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M700to800_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M1000to1500_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M1500to2000_TauTau_EE.root",
-	"output_data/DYHists_v2p6_TauTau_DYLL_M2000to3000_TauTau_EE.root"
+	"output_data/DYLL_M1000to1500_TauTau.root",
+	"output_data/DYLL_M100to200_TauTau.root",
+	"output_data/DYLL_M10to50_TauTau.root",
+	"output_data/DYLL_M1500to2000_TauTau.root",
+	"output_data/DYLL_M2000to3000_TauTau.root",
+	"output_data/DYLL_M200to400_TauTau.root",
+	"output_data/DYLL_M400to500_TauTau.root",
+	"output_data/DYLL_M500to700_TauTau.root",
+	"output_data/DYLL_M50to100_TauTau.root",
+	"output_data/DYLL_M700to800_TauTau.root",
+	"output_data/DYLL_M800to1000_TauTau.root"
 };
 
 vector<TString> file_DYLL = {
-	"output_data/DYHists_v2p6_DYtoLL_M1000to1500_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M100to200_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M10to50_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M1500to2000_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M2000to3000_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M200to400_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M400to500_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M500to700_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M50to100_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M700to800_EE.root",
-	"output_data/DYHists_v2p6_DYtoLL_M800to1000_EE.root"
+	"output_data/DYLL_M1000to1500_EE.root",
+	"output_data/DYLL_M100to200_EE.root",
+	"output_data/DYLL_M10to50_EE.root",
+	"output_data/DYLL_M1500to2000_EE.root",
+	"output_data/DYLL_M2000to3000_EE.root",
+	"output_data/DYLL_M200to400_EE.root",
+	"output_data/DYLL_M400to500_EE.root",
+	"output_data/DYLL_M500to700_EE.root",
+	"output_data/DYLL_M50to100_EE.root",
+	"output_data/DYLL_M700to800_EE.root",
+	"output_data/DYLL_M800to1000_EE.root"
 };
 
 vector<TString> file_top = {
-	"output_data/DYHists_v2p6_Top_ST_tW_EE.root",
-	"output_data/DYHists_v2p6_Top_ST_tbarW_EE.root",
-	"output_data/DYHists_v2p6_Top_ttbar_M0to700_EE.root",
-	"output_data/DYHists_v2p6_Top_ttbar_M1000toInf_EE.root",
-	"output_data/DYHists_v2p6_Top_ttbar_M700to1000_EE.root"
+	"output_data/ST_tW.root",
+	"output_data/ST_tbarW.root",
+	"output_data/ttbar_M0to700.root",
+	"output_data/ttbar_M1000toInf.root",
+	"output_data/ttbar_M700to1000.root"
 };
 
 vector<TString> file_Fake = {
-	"output_data/DYHists_v2p6_Fake_WJetsToLNuExt_EE.root",
-	"output_data/DYHists_v2p6_Fake_WJetsToLNu_EE.root"
+	"output_data/WJetsToLNu_amcatnlo.root"
 };
 
 TH1D*GetHistogram(vector<TString> filesvector);
@@ -82,7 +84,7 @@ void makeStackPlots()
 	hStack_Mass->Draw("hist");
 	hMassData->Draw("pe,same");
 
-	c1->SaveAs("tempDataVsMCStack.png");
+	c1->SaveAs("plots/tempDataVsMCStack.png");
 }
 
 TH1D*GetHistogram(vector<TString> filesvector)
