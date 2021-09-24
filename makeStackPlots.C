b537c3601f7b43686d366c8048cbe351fff549ab
Robert Tabb <wrtabb@gmail.com>
2021-09-24 14:56:03 -0500

    updated plotting script to make ratio plot

--- makeStackPlots.C
+++ makeStackPlots.C
@@ -1,22 +1,19 @@
 
 vector<TString> file_data = {
-	"output_data/saveFile_MuMu_SingleMuon_Run2016B.root",           // 0
-	"output_data/saveFile_MuMu_SingleMuon_Run2016C.root",           // 1
-	"output_data/saveFile_MuMu_SingleMuon_Run2016D.root",           // 2
-	"output_data/saveFile_MuMu_SingleMuon_Run2016E.root",           // 3
-	"output_data/saveFile_MuMu_SingleMuon_Run2016F.root",           // 4
-	"output_data/saveFile_MuMu_SingleMuon_Run2016G.root",           // 5
-	"output_data/saveFile_MuMu_SingleMuon_Run2016Hver2.root",       // 6
-	"output_data/saveFile_MuMu_SingleMuon_Run2016Hver3.root"       // 7
+	"output_data/saveFile_MuMu_SingleMuon_Run2016B.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016C.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016D.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016E.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016F.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016G.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016Hver2.root",
+	"output_data/saveFile_MuMu_SingleMuon_Run2016Hver3.root"
 };
 
-vector<TString> file_diboson = {
+vector<TString> file_ew= {
 	"output_data/saveFile_MuMu_WW.root",
 	"output_data/saveFile_MuMu_WZ.root",
-	"output_data/saveFile_MuMu_ZZ.root"
-};
-
-vector<TString> file_taus = {
+	"output_data/saveFile_MuMu_ZZ.root",
 	"output_data/saveFile_MuMu_DYLL_M10to50_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M50to100_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M100to200_TauTau.root",
@@ -24,7 +21,7 @@ vector<TString> file_taus = {
 	"output_data/saveFile_MuMu_DYLL_M400to500_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M500to700_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M700to800_TauTau.root",
-	"output_data/saveFile_MuMu_DYLL_M800to1000_TauTau.root",
+	"output_data/saveFile_MuMu_DYLL_M800to1000_TauTau.root"
 	"output_data/saveFile_MuMu_DYLL_M1000to1500_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M1500to2000_TauTau.root",
 	"output_data/saveFile_MuMu_DYLL_M2000to3000_TauTau.root"
@@ -57,14 +54,14 @@ vector<TString> file_Fake = {
 };
 
 vector<TString> hist_data = {
-	"SingleMuon_Run2016B",           // 0
-	"SingleMuon_Run2016C",           // 1
-	"SingleMuon_Run2016D",           // 2
-	"SingleMuon_Run2016E",           // 3
-	"SingleMuon_Run2016F",           // 4
-	"SingleMuon_Run2016G",           // 5
-	"SingleMuon_Run2016Hver2",       // 6
-	"SingleMuon_Run2016Hver3"       // 7
+	"SingleMuon_Run2016B",
+	"SingleMuon_Run2016C",
+	"SingleMuon_Run2016D",
+	"SingleMuon_Run2016E",
+	"SingleMuon_Run2016F",
+	"SingleMuon_Run2016G",
+	"SingleMuon_Run2016Hver2",
+	"SingleMuon_Run2016Hver3"
 };
 vector<TString> hist_DYLL = {
         "DYLL_M10to50_MuMu",              // 8
@@ -87,12 +84,10 @@ vector<TString> hist_top = {
         "ttbar_M700to1000",             // 22
         "ttbar_M1000toInf"              // 23
 };
-vector<TString> hist_diboson = {
+vector<TString> hist_ew= {
         "WW",                           // 24
         "WZ",                           // 25
-        "ZZ"                            // 26
-};
-vector<TString> hist_taus = {
+        "ZZ",                           // 26
         "DYLL_M10to50_TauTau",          // 27
         "DYLL_M50to100_TauTau",         // 28
         "DYLL_M100to200_TauTau",        // 29
@@ -109,221 +104,211 @@ vector<TString> hist_Fake = {
         "WJetsToLNu_amcatnlo"           // 38   
 };
 
+enum Variable{
+	INV_MASS,
+	RAPIDITY,
+	PT_LEAD,
+	PT_SUB,
+	ETA_LEAD,
+	ETA_SUB,
+	ERR
+};
+
 TH1D*GetHistogram(vector<TString> filesvector,vector<TString> histvector,TString variable);
+vector<TString> GetPlotProperties(Variable var);
+void MakePlots(Variable var);
 
 void makeStackPlots()
 {
 	//gROOT->SetBatch(true);
-	TCanvas*c1 = new TCanvas("c1","",0,0,1000,1000);
-	c1->SetGrid();
-	c1->SetLogx();
-	c1->SetLogy();
+	gStyle->SetOptStat(0);
+	MakePlots(INV_MASS);
+	MakePlots(RAPIDITY);
+	MakePlots(PT_LEAD);
+	MakePlots(PT_SUB);
 
-	TH1D*hMassDYLL = GetHistogram(file_DYLL,hist_DYLL,"InvMass");
-	hMassDYLL->SetFillColor(kOrange-2);
-	hMassDYLL->SetLineColor(kOrange+3);
-	TH1D*hMassTops = GetHistogram(file_top,hist_top,"InvMass");
-	hMassTops->SetFillColor(kBlue+2);
-	hMassTops->SetLineColor(kBlue+3);
-	TH1D*hMassFake = GetHistogram(file_Fake,hist_Fake,"InvMass");
-	hMassFake->SetFillColor(kViolet+5);
-	hMassFake->SetLineColor(kViolet+3);
-	TH1D*hMassDiboson = GetHistogram(file_diboson,hist_diboson,"InvMass");
-	hMassDiboson->SetFillColor(kRed+2);
-	hMassDiboson->SetLineColor(kRed+4);
-	TH1D*hMassTau = GetHistogram(file_taus,hist_taus,"InvMass");
-	hMassTau->SetFillColor(kGreen+2);
-	hMassTau->SetLineColor(kGreen+3);
+}
 
-	THStack*hStackInvMass = new THStack("hStackInvMass","");
-	hStackInvMass->Add(hMassFake);
-	hStackInvMass->Add(hMassDiboson);
-	hStackInvMass->Add(hMassTau);
-	hStackInvMass->Add(hMassTops);
-	hStackInvMass->Add(hMassDYLL);
+void MakePlots(Variable var)
+{
+	vector<TString> plotProperties = GetPlotProperties(var);
+
+	// MC Signal
+	TH1D*hDYLL = GetHistogram(file_DYLL,hist_DYLL,plotProperties.at(0));
+	hDYLL->SetFillColor(kOrange-2);
+	hDYLL->SetLineColor(kOrange+3);
+	// Top quarks
+	TH1D*hTops = GetHistogram(file_top,hist_top,plotProperties.at(0));
+	hTops->SetFillColor(kBlue+2);
+	hTops->SetLineColor(kBlue+3);
+	// W+Jets
+	TH1D*hFake = GetHistogram(file_Fake,hist_Fake,plotProperties.at(0));
+	hFake->SetFillColor(kViolet+5);
+	hFake->SetLineColor(kViolet+3);
+	// EW
+	TH1D*hEW = GetHistogram(file_ew,hist_ew,plotProperties.at(0));
+	hEW->SetFillColor(kRed+2);
+	hEW->SetLineColor(kRed+4);
+
+	// Total MC sum
+	TString hSumName = "hSum";
+	hSumName += plotProperties.at(0); 
+	TH1D*hSum = (TH1D*)hDYLL->Clone(hSumName);
+	hSum->Add(hTops);
+	hSum->Add(hFake);
+	hSum->Add(hEW);
+
+	// Place signal and background into a stack
+	TString stackName = "hStack";
+	stackName += plotProperties.at(0);
+	THStack*hStack = new THStack(stackName,"");
+	hStack->Add(hFake);
+	hStack->Add(hEW);
+	hStack->Add(hTops);
+	hStack->Add(hDYLL);
 	
-	TH1D*hDataInvMass = GetHistogram(file_data,hist_data,"InvMass");
-	hDataInvMass->SetMarkerStyle(20);
-	hDataInvMass->SetMarkerColor(kBlack);
+	// Data
+	TH1D*hData = GetHistogram(file_data,hist_data,plotProperties.at(0));
+	hData->SetMarkerStyle(20);
+	hData->SetMarkerColor(kBlack);
 
-	hStackInvMass->SetMinimum(0.01);
-	hStackInvMass->Draw("hist");
-	hDataInvMass->Draw("pe,same");
-	hStackInvMass->GetXaxis()->SetTitle("invariant mass [GeV]");
-	hStackInvMass->SetTitle("Dimuon invariant mass");
-	hStackInvMass->GetXaxis()->SetMoreLogLabels();
-	hStackInvMass->GetXaxis()->SetNoExponent();
-	c1->Update();
+	// Data over MC ratio
+	TString ratioName = "ratio";
+	ratioName += plotProperties.at(0);
+	TH1D*hRatio = (TH1D*)hData->Clone(ratioName);
+	hRatio->Divide(hSum);
+
+	int nBins = hDYLL->GetNbinsX();
+	double binWidthLastBin = hDYLL->GetBinWidth(nBins);
+	float x1 = hDYLL->GetBinLowEdge(0);
+	float x2 = hDYLL->GetBinLowEdge(nBins)+binWidthLastBin;
+
+	TLine*line = new TLine(x1,1,x2,1);
+	line->SetLineColor(kRed);
+
+	// Canvas
+	TString canvasName = "canvas";
+	canvasName += plotProperties.at(0);
+	TCanvas*canvas = new TCanvas(canvasName,"",0,0,1000,1000);
+
+	// Upper pad
+	TPad*pad1 = new TPad("","",0,0.3,1,1.0);
+	pad1->SetBottomMargin(0.03);
+	pad1->SetGrid();
+	pad1->SetLogy();
+	pad1->SetTicks(1,1);
+	pad1->Draw();
+	pad1->cd();
+	if(var==INV_MASS) pad1->SetLogx();
+	hStack->Draw("hist");
+
+	hStack->SetMinimum(0.01);
+	hStack->SetMaximum(100000000);
+	hStack->Draw("hist");
+	hData->Draw("pe,same");
 
 	TLegend*legend = new TLegend(0.65,0.9,0.9,0.75);
 	legend->SetTextSize(0.02);
-	legend->AddEntry(hDataInvMass,"Data");
-	legend->AddEntry(hMassDYLL,"#gamma^{*}/Z #rightarrow e^{-}e^{+}");
-	legend->AddEntry(hMassTops,"t#bar{t}+tW+#bar{t}W");
-	legend->AddEntry(hMassTau,"#gamma^{*}/Z #rightarrow #tau^{-}#tau^{+}");
-	legend->AddEntry(hMassDiboson,"WW,WZ,ZZ");
-	legend->AddEntry(hMassFake,"W+Jets");
+	legend->AddEntry(hData,"Data");
+	legend->AddEntry(hDYLL,"#gamma^{*}/Z #rightarrow e^{-}e^{+}");
+	legend->AddEntry(hTops,"t#bar{t}+tW+#bar{t}W");
+	legend->AddEntry(hEW,"diboson + #gamma^{*}/Z #rightarrow #tau^{-}#tau^{+}");
+	legend->AddEntry(hFake,"W+Jets");
 	legend->Draw("same");
 
-	c1->SaveAs("plots/dataVsMC_MuMu_InvMass.png");
+	// Stack properties
+	hStack->GetXaxis()->SetTitle(plotProperties.at(2));
+	hStack->GetXaxis()->SetMoreLogLabels();
+	hStack->GetXaxis()->SetNoExponent();
+	hStack->SetTitle(plotProperties.at(1));
+	hStack->SetMinimum(0.1);
+	hStack->GetXaxis()->SetLabelSize(0);
+	hStack->GetXaxis()->SetTitleSize(0);
+	canvas->Update();
+	hData->Draw("pe,same");
+	legend->Draw("same");
 
-	// Rapidity
-	TCanvas*c2 = new TCanvas("c2","",0,0,1000,1000);
-	c2->SetGrid();
-	c2->SetLogy();
+	canvas->cd();
 
-	TH1D*hRapidityDYLL = GetHistogram(file_DYLL,hist_DYLL,"Rapidity");
-	hRapidityDYLL->SetFillColor(kOrange-2);
-	hRapidityDYLL->SetLineColor(kOrange+3);
-	TH1D*hRapidityTops = GetHistogram(file_top,hist_top,"Rapidity");
-	hRapidityTops->SetFillColor(kBlue+2);
-	hRapidityTops->SetLineColor(kBlue+3);
-	TH1D*hRapidityFake = GetHistogram(file_Fake,hist_Fake,"Rapidity");
-	hRapidityFake->SetFillColor(kViolet+5);
-	hRapidityFake->SetLineColor(kViolet+3);
-	TH1D*hRapidityDiboson = GetHistogram(file_diboson,hist_diboson,"Rapidity");
-	hRapidityDiboson->SetFillColor(kRed+2);
-	hRapidityDiboson->SetLineColor(kRed+4);
-	TH1D*hRapidityTau = GetHistogram(file_taus,hist_taus,"Rapidity");
-	hRapidityTau->SetFillColor(kGreen+2);
-	hRapidityTau->SetLineColor(kGreen+3);
+	// Lower pad
+	TPad*pad2 = new TPad("","",0,0.05,1,0.3);	
+	if(var==INV_MASS) pad2->SetLogx();
+	pad2->SetTopMargin(0.03);
+	pad2->SetBottomMargin(0.2);
+	pad2->SetGrid();
+	pad2->SetTicks(1,1);
+	pad2->Draw();
+	pad2->cd();
 
-	THStack*hStackRapidity = new THStack("hStackRapidity","");
-	hStackRapidity->Add(hRapidityFake);
-	hStackRapidity->Add(hRapidityDiboson);
-	hStackRapidity->Add(hRapidityTau);
-	hStackRapidity->Add(hRapidityTops);
-	hStackRapidity->Add(hRapidityDYLL);
-	
-	TH1D*hRapidityData = GetHistogram(file_data,hist_data,"Rapidity");
-	hRapidityData->SetMarkerStyle(20);
-	hRapidityData->SetMarkerColor(kBlack);
+	hRatio->GetYaxis()->SetLabelSize(0.06);
+	hRatio->GetYaxis()->SetTitleSize(0.08);
+	hRatio->GetYaxis()->SetTitleOffset(0.3);
+	hRatio->GetYaxis()->SetTitle("Data/MC");
+	hRatio->GetXaxis()->SetLabelSize(0.1);
+	hRatio->GetXaxis()->SetTitleSize(0.1);
+	hRatio->GetXaxis()->SetNoExponent();
+	hRatio->GetXaxis()->SetMoreLogLabels();
+	hRatio->SetMaximum(1.3);
+	hRatio->SetMinimum(0.7);
+	hRatio->GetXaxis()->SetTitle(plotProperties.at(1));
+	hRatio->Draw("PE");
+	line->Draw("same");
+	canvas->Update();
 
-	hStackRapidity->SetMaximum(10000000);
-	hStackRapidity->SetMinimum(0.01);
-	hStackRapidity->Draw("hist");
-	hRapidityData->Draw("pe,same");
-	hStackRapidity->GetXaxis()->SetTitle("Dimuon rapidity");
-	hStackRapidity->SetTitle("Dimuon rapidity");
-	c2->Update();
+	TString saveName = "plots/dataVsMC_MuMu_";
+	saveName += plotProperties.at(0);
+	saveName += ".png";
+	canvas->SaveAs(saveName);
+}
 
-	TLegend*legend2 = new TLegend(0.65,0.9,0.9,0.75);
-	legend2->SetTextSize(0.02);
-	legend2->AddEntry(hRapidityData,"Data");
-	legend2->AddEntry(hRapidityDYLL,"#gamma^{*}/Z #rightarrow e^{-}e^{+}");
-	legend2->AddEntry(hRapidityTops,"t#bar{t}+tW+#bar{t}W");
-	legend2->AddEntry(hRapidityTau,"#gamma^{*}/Z #rightarrow #tau^{-}#tau^{+}");
-	legend2->AddEntry(hRapidityDiboson,"WW,WZ,ZZ");
-	legend2->AddEntry(hRapidityFake,"W+Jets");
-	legend2->Draw("same");
+vector<TString> GetPlotProperties(Variable var)
+{
+	vector<TString> properties;
+	vector<TString> errorVec = {"ERROR"};
 
-	c2->SaveAs("plots/dataVsMC_MuMu_Rapidity.png");
+	TString varTag;
+	TString plotTitle;
+	TString axisTitle;
 
-	// pT Lead
-	TCanvas*c3 = new TCanvas("c3","",0,0,1000,1000);
-	c3->SetGrid();
-	c3->SetLogy();
+	if(var==INV_MASS){
+		varTag = "InvMass";
+		plotTitle = "Dimuon invariant mass";
+		axisTitle = "invariant mass [GeV]";		
+	}
+	else if(var==RAPIDITY){
+		varTag = "Rapidity";
+		plotTitle = "Dimuon rapidity";
+		axisTitle = "rapidity";		
+	}
+	else if(var==PT_LEAD){
+		varTag = "PtLead";
+		plotTitle = "Leading muon p_{T}";
+		axisTitle = "p_{T} [GeV]";		
+	}
+	else if(var==PT_SUB){
+		varTag = "PtSub";
+		plotTitle = "Sub-leading muon p_{T}";
+		axisTitle = "p_{T} [GeV]";		
+	}
+	else if(var==ETA_LEAD){
+		varTag = "EtaLead";
+		plotTitle = "Leading muon #eta";
+		axisTitle = "#eta";		
+	}
+	else if(var==ETA_SUB){
+		varTag = "EtaSub";
+		plotTitle = "Sub-leading muon #eta";
+		axisTitle = "#eta";		
+	}
+	else{
+		cout << "Variable not properly set in MakePlots()" << endl;
+		return errorVec;
+	}
+	properties.push_back(varTag);
+	properties.push_back(plotTitle);
+	properties.push_back(axisTitle);
 
-	TH1D*hPtLeadDYLL = GetHistogram(file_DYLL,hist_DYLL,"PtLead");
-	hPtLeadDYLL->SetFillColor(kOrange-2);
-	hPtLeadDYLL->SetLineColor(kOrange+3);
-	TH1D*hPtLeadTops = GetHistogram(file_top,hist_top,"PtLead");
-	hPtLeadTops->SetFillColor(kBlue+2);
-	hPtLeadTops->SetLineColor(kBlue+3);
-	TH1D*hPtLeadFake = GetHistogram(file_Fake,hist_Fake,"PtLead");
-	hPtLeadFake->SetFillColor(kViolet+5);
-	hPtLeadFake->SetLineColor(kViolet+3);
-	TH1D*hPtLeadDiboson = GetHistogram(file_diboson,hist_diboson,"PtLead");
-	hPtLeadDiboson->SetFillColor(kRed+2);
-	hPtLeadDiboson->SetLineColor(kRed+4);
-	TH1D*hPtLeadTau = GetHistogram(file_taus,hist_taus,"PtLead");
-	hPtLeadTau->SetFillColor(kGreen+2);
-	hPtLeadTau->SetLineColor(kGreen+3);
-
-	THStack*hStackPtLead = new THStack("hStackPtLead","");
-	hStackPtLead->Add(hPtLeadFake);
-	hStackPtLead->Add(hPtLeadDiboson);
-	hStackPtLead->Add(hPtLeadTau);
-	hStackPtLead->Add(hPtLeadTops);
-	hStackPtLead->Add(hPtLeadDYLL);
-	
-	TH1D*hPtLeadData = GetHistogram(file_data,hist_data,"PtLead");
-	hPtLeadData->SetMarkerStyle(20);
-	hPtLeadData->SetMarkerColor(kBlack);
-
-	hStackPtLead->SetMaximum(10000000);
-	hStackPtLead->SetMinimum(0.01);
-	hStackPtLead->Draw("hist");
-	hPtLeadData->Draw("pe,same");
-
-	hStackPtLead->SetTitle("Leading muon p_{T}");
-	hStackPtLead->GetXaxis()->SetTitle("p_{T} [GeV]");
-	c3->Update();
-
-	TLegend*legend3 = new TLegend(0.65,0.9,0.9,0.75);
-	legend3->SetTextSize(0.02);
-	legend3->AddEntry(hPtLeadData,"Data");
-	legend3->AddEntry(hPtLeadDYLL,"#gamma^{*}/Z #rightarrow e^{-}e^{+}");
-	legend3->AddEntry(hPtLeadTops,"t#bar{t}+tW+#bar{t}W");
-	legend3->AddEntry(hPtLeadTau,"#gamma^{*}/Z #rightarrow #tau^{-}#tau^{+}");
-	legend3->AddEntry(hPtLeadDiboson,"WW,WZ,ZZ");
-	legend3->AddEntry(hPtLeadFake,"W+Jets");
-	legend3->Draw("same");
-
-	c3->SaveAs("plots/dataVsMC_MuMu_PtLead.png");
-
-	// pT Sublead
-	TCanvas*c4 = new TCanvas("c4","",0,0,1000,1000);
-	c4->SetGrid();
-	c4->SetLogy();
-
-	TH1D*hPtSubDYLL = GetHistogram(file_DYLL,hist_DYLL,"PtSub");
-	hPtSubDYLL->SetFillColor(kOrange-2);
-	hPtSubDYLL->SetLineColor(kOrange+3);
-	TH1D*hPtSubTops = GetHistogram(file_top,hist_top,"PtSub");
-	hPtSubTops->SetFillColor(kBlue+2);
-	hPtSubTops->SetLineColor(kBlue+3);
-	TH1D*hPtSubFake = GetHistogram(file_Fake,hist_Fake,"PtSub");
-	hPtSubFake->SetFillColor(kViolet+5);
-	hPtSubFake->SetLineColor(kViolet+3);
-	TH1D*hPtSubDiboson = GetHistogram(file_diboson,hist_diboson,"PtSub");
-	hPtSubDiboson->SetFillColor(kRed+2);
-	hPtSubDiboson->SetLineColor(kRed+4);
-	TH1D*hPtSubTau = GetHistogram(file_taus,hist_taus,"PtSub");
-	hPtSubTau->SetFillColor(kGreen+2);
-	hPtSubTau->SetLineColor(kGreen+3);
-
-	THStack*hStackPtSub = new THStack("hStackPtSub","");
-	hStackPtSub->Add(hPtSubFake);
-	hStackPtSub->Add(hPtSubDiboson);
-	hStackPtSub->Add(hPtSubTau);
-	hStackPtSub->Add(hPtSubTops);
-	hStackPtSub->Add(hPtSubDYLL);
-	
-	TH1D*hPtSubData = GetHistogram(file_data,hist_data,"PtSub");
-	hPtSubData->SetMarkerStyle(20);
-	hPtSubData->SetMarkerColor(kBlack);
-
-	hStackPtSub->SetMaximum(10000000);
-	hStackPtSub->SetMinimum(0.01);
-	hStackPtSub->Draw("hist");
-	hPtSubData->Draw("pe,same");
-	hStackPtSub->SetTitle("Sub-leading muon p_{T}");
-	hStackPtSub->GetXaxis()->SetTitle("p_{T} [GeV]");
-	c4->Update();
-
-	TLegend*legend4 = new TLegend(0.65,0.9,0.9,0.75);
-	legend4->SetTextSize(0.02);
-	legend4->AddEntry(hPtSubData,"Data");
-	legend4->AddEntry(hPtSubDYLL,"#gamma^{*}/Z #rightarrow e^{-}e^{+}");
-	legend4->AddEntry(hPtSubTops,"t#bar{t}+tW+#bar{t}W");
-	legend4->AddEntry(hPtSubTau,"#gamma^{*}/Z #rightarrow #tau^{-}#tau^{+}");
-	legend4->AddEntry(hPtSubDiboson,"WW,WZ,ZZ");
-	legend4->AddEntry(hPtSubFake,"W+Jets");
-	legend4->Draw("same");
-
-	c4->SaveAs("plots/dataVsMC_MuMu_PtSub.png");
-	
+	return properties;	
 }
 
 TH1D*GetHistogram(vector<TString> filesvector,vector<TString> histvector,TString variable)
