6c6269f8aaf9edb973ea0818bf2d6718dc9079b7
Robert Tabb <wrtabb@gmail.com>
2021-11-09 10:52:38 -0600

    fixed unfolding bugs

--- Unfolding/unfold.C
+++ Unfolding/unfold.C
@@ -1,4 +1,3 @@
-#include "include/ToyModel.hh"
 #include "include/Unfolding.hh"
 
 TString inputDistribution = "../data/unfoldingHistograms";
@@ -28,24 +27,25 @@ void unfold()
 	Unfold*unfold = new Unfold();
 	Unfold::RegType regType = unfold->NO_REG;
 
-	TH1F*hUnfoldedClosure;
-	TH1F*hUnfolded;
-	//hUnfoldedClosure = unfold->unfoldTUnfold(regType,hReco,hBack,hTrue,hMatrix);
-	hUnfolded = unfold->unfoldTUnfold(regType,hData,hBack,hTrue,hMatrix);
-	hUnfolded->SetMarkerStyle(25);
-	hUnfolded->SetMarkerColor(kBlue+2);
-	hUnfolded->SetLineColor(kBlue+2);
+	hReco->Add(hBack);
+	TH1F*hUnfoldClosure = unfold->unfoldTUnfold(regType,hReco,hBack,hTrue,hMatrix);
+	TH1F*hUnfold = unfold->unfoldTUnfold(regType,hData,hBack,hTrue,hMatrix);
 
 	bool logPlot = true;
-	TCanvas*c1 = unfold->plotUnfolded("c1","Unfold Data",hData,hTrue,hUnfoldedClosure,logPlot);
-
+	TCanvas*c1 = 
+		unfold->plotUnfolded("c1","Closure Test",hReco,hTrue,hUnfoldClosure,logPlot);
+	TCanvas*c2 = 
+		unfold->plotUnfolded("c2","Data Unfold",hData,hTrue,hUnfold,logPlot);
+	c1->SaveAs("../plots/unfoldedEE_ClosureTest.png");
+	c2->SaveAs("../plots/unfoldedEE_Data.png");
+/*
 	TH2F*hResponse = unfold->makeResponseMatrix(hMatrix);
 	TCanvas*c2 = new TCanvas("c2","",0,0,1000,1000);
 	c2->SetGrid();
 	c2->SetLogy();
 	c2->SetLogx();
 	hResponse->Draw("colz");
-
+*/
 //	TH1F*hUnfoldInv;
 //	hUnfoldInv = unfold->unfoldInversion(hReco,hTrue,hMatrix);
 //	TCanvas*c3 = unfold->plotUnfolded("c3","Unfold Reco Inv",hReco,hTrue,hUnfolded,logPlot);
